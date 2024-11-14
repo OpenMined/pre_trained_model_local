3f21c4d96e4bb8cd40e2a2835e3f06a3ce8211e7
Ionésio Junior <ionesiojr@gmail.com>
2024-11-14 17:37:16 -0300

    Update run.sh
    - Remove syftbox installation from if scope
    - Replace uv run python3 main.py -> python3 main.py

--- run.sh
+++ run.sh
@@ -5,17 +5,18 @@ if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv -p 3.12 .venv
     echo "Virtual environment created successfully."
-    uv pip install -U syftbox
+
     # uv pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
 fi
 
+uv pip install -U syftbox
 . .venv/bin/activate
 
 # # run app using python from venv
 echo "Running pre_trained_model_local with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+python3 main.py
 
 # # deactivate the virtual environment
 deactivate
